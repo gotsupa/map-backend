952cc53f6e630da155a7a2f3e61880bfde52ad2f
gotsupa 🐛 <supachai.chanu@gmail.com>
2022-09-25 18:15:21 +0700

    fix config file for env

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ import cors from 'cors'
 import axios from 'axios'
 import * as dotenv from 'dotenv'
 
-import { corsOptions, PORT, GOOGLE_API_KEY } from './config'
-
-dotenv.config()
+import { corsOptions } from './config.js'
 
 const app = express()
 
+dotenv.config()
 app.use(express.json())
 app.use(cors(corsOptions))
 
+const { UNIQUE_API_KEY = '', PORT = 3000 } = process.env
+
 const getLocation = async (place) => {
-  const URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${place}&language=th&type=restaurant&key=${GOOGLE_API_KEY}`
+  const URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${place}&language=th&type=restaurant&key=${UNIQUE_API_KEY}`
 
   const response = await axios({
     method: 'get',
--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-export const PORT = process.env.PORT || 3000
-export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || ''
-
 export const corsOptions = {
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
